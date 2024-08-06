7bd94913ffab6ce738bb017f62f2e022c1f44d72
B, Prashanth Natraj <Prashanth.Natraj.B@intel.com>
2024-08-06 07:10:08 +0530

    Enable CI for smoke test and pycloudstack runs
    Signed-off-by: B, Prashanth Natraj <Prashanth.Natraj.B@intel.com>

--- ci/tdx_canonical_linux_stack.sh
+++ ci/tdx_canonical_linux_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_name=$(basename "$0")
-long=setuptdx,verifytdx,createtd,runtdqemu,runtdlibvirt
+long=setuptdx,verifytdx,createtd,runtdqemu,runtdlibvirt,smoke,automatedtests
 TEMP=$(getopt -l $long -n $script_name -- "$@")
 CUR_DIR=$(pwd)
 TDX_DIR="$CUR_DIR"/tdx
@@ -22,14 +22,14 @@ if [[ $DISTRO_VER == "23.10" ]]; then
         QCOW2_IMG="$GUEST_IMG_DIR"/tdx-guest-ubuntu-23.10.qcow2
         BRANCH_NAME=mantic-23.10
         TD_VIRSH_BOOT_CMD=td_virsh_tool.sh
-        TD_VIRSH_DELETE_CMD="$TD_VIRSH_BOOT_CMD -c all"        
+        TD_VIRSH_DELETE_CMD="$TD_VIRSH_BOOT_CMD -c all"
 fi
 
 setuptdx(){
         [ -d $TDX_DIR ] && rm -rf $TDX_DIR
         git clone -b $BRANCH_NAME https://github.com/canonical/tdx.git $TDX_DIR
         cd $TDX_DIR
-        sudo ./setup-tdx-host.sh | tee setup_tdx.log
+        ./setup-tdx-host.sh | tee setup_tdx.log
         if grep -viq "${RESTART_CHECK_STRING}" setup_tdx.log; then
                 echo "system restart skipped"
         else
@@ -39,7 +39,7 @@ setuptdx(){
 }
 
 verifytdx(){
-        var="$(sudo dmesg | grep -i tdx)"
+        var="$(dmesg | grep -i tdx)"
         echo "$var"
         if [[ "$var" =~ "${TDT_HOST_VERIFY_TEXT}" ]]; then
                 echo "tdx is configured on the Host"
@@ -52,12 +52,11 @@ verifytdx(){
 createtd(){
         echo $GUEST_IMG_DIR
         cd "$GUEST_IMG_DIR"
-        sudo ./create-td-image.sh
-	sudo virt-get-kernel -a $QCOW2_IMG
-	VMLINUZ="$GUEST_IMG_DIR"/"$(ls | grep vmlinuz)"
-	echo "$VMLINUZ"
+        ./create-td-image.sh
+        virt-get-kernel -a $QCOW2_IMG
+        VMLINUZ="$GUEST_IMG_DIR"/"$(ls | grep vmlinuz)"
+        echo "$VMLINUZ"
         cd "$TDX_DIR"
-        sudo ./setup-tdx-guest.sh
 }
 
 verifytd(){
@@ -74,14 +73,14 @@ verifytd(){
 }
 
 cleanup(){
-        sudo ./$TD_VIRSH_DELETE_CMD
+        ./$TD_VIRSH_DELETE_CMD
         fuser -k $QCOW2_IMG
         sleep 20
 }
 
 runtdqemu(){
         echo "creating TD guest with QEMU"
-        sudo usermod -aG kvm $USER
+        usermod -aG kvm $USER
         cd "$GUEST_TOOLS_DIR"
         cleanup
         var=$( ./run_td.sh)
@@ -97,13 +96,13 @@ runtdqemu(){
 
 runtdlibvirt(){
         echo "creating TD guest with libvirt"
-        sudo grep -q 'user =' "$LIBVIRT_CONF" && sudo sed 's/^user =.*/user = \"root\"/' -i "$LIBVIRT_CONF" || sudo sh -c "echo 'user = "root"' >> $LIBVIRT_CONF"
-        sudo grep -q 'group =' "$LIBVIRT_CONF" && sudo sed 's/^group =.*/group = \"root\"/' -i "$LIBVIRT_CONF" || sudo sh -c "echo 'group = "root"' >> $LIBVIRT_CONF"
-        sudo grep -q 'dynamic_ownership =' "$LIBVIRT_CONF" && sudo sed 's/^dynamic_ownership =.*/dynamic_ownership = 0/' -i "$LIBVIRT_CONF" || sudo sh -c "echo 'dynamic_ownership = 0' >> $LIBVIRT_CONF"
-        sudo systemctl restart libvirtd
+        grep -q 'user =' "$LIBVIRT_CONF" && sed 's/^user =.*/user = \"root\"/' -i "$LIBVIRT_CONF" || sh -c "echo 'user = "root"' >> $LIBVIRT_CONF"
+        grep -q 'group =' "$LIBVIRT_CONF" && sed 's/^group =.*/group = \"root\"/' -i "$LIBVIRT_CONF" || sh -c "echo 'group = "root"' >> $LIBVIRT_CONF"
+        grep -q 'dynamic_ownership =' "$LIBVIRT_CONF" && sed 's/^dynamic_ownership =.*/dynamic_ownership = 0/' -i "$LIBVIRT_CONF" || sh -c "echo 'dynamic_ownership = 0' >> $LIBVIRT_CONF"
+        systemctl restart libvirtd
         cd "$GUEST_TOOLS_DIR"
         cleanup
-        var=$(sudo ./${TD_VIRSH_BOOT_CMD})
+        var=$(./${TD_VIRSH_BOOT_CMD})
         ret=$?
         echo $var
         if [ $ret -ne 0 ]; then
@@ -112,44 +111,47 @@ runtdlibvirt(){
         sleep 20
         echo "verifying TD guest on libvirt"
         if [[ $DISTRO_VER == "23.10" ]]; then
-                port_num=$(echo $var | awk -F '-p' '{print $2}' | cut -d ' ' -f 2)                
+                port_num=$(echo $var | awk -F '-p' '{print $2}' | cut -d ' ' -f 2)
         else
-                port_num=$(echo $(sudo ./tdvirsh list --all) | awk -F 'ssh:' '{print $2}' | cut -d ',' -f 1)    
+                port_num=$(echo $(./tdvirsh list --all) | awk -F 'ssh:' '{print $2}' | cut -d ',' -f 1)
         fi
         verifytd $port_num
 }
 
 setup_pycloudstack(){
-	echo "Installing PyCloudStack required packages in Guest image"
-	sudo LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1 virt-customize -a $QCOW2_IMG --install qemu-utils,libguestfs-tools,cpuid,python3-virtualenv,python3-libvirt,libguestfs-dev,libvirt-dev,python3-dev,net-tools,qemu-guest-agent,docker.io,cgroupfs-mount --run-command 'cgroupfs-mount' --run-command 'dockerd -D &' --run-command "docker pull redis" --run-command "docker pull nginx"
-	echo "Installing PyCloudStack required packages on host system"
-	sudo apt install -y python3-virtualenv python3-libvirt libguestfs-dev libvirt-dev python3-dev net-tools
-	sudo usermod -aG libvirt root
-	sudo systemctl restart libvirtd
-	echo "Setting up PyCloudStack venv"
-	[ -d $TDX_TOOLS_DIR ] && sudo rm -rf $TDX_TOOLS_DIR
-	git clone https://github.com/anjalirai-intel/tdx-tools.git $TDX_TOOLS_DIR
-	cd $TDX_TOOLS_DIR/tests/
-	source setupenv.sh
-	ssh-keygen -f tests/vm_ssh_test_key -N ""
-	cat >artifacts.yaml <<EOL
+        echo "Installing PyCloudStack required packages in Guest image"
+        LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1 virt-customize -a $QCOW2_IMG --install qemu-utils,libguestfs-tools,cpuid,python3-virtualenv,python3-libvirt,libguestfs-dev,libvirt-dev,python3-dev,net-tools,qemu-guest-agent,docker.io,cgroupfs-mount --run-command 'cgroupfs-mount' --run-command 'dockerd -D &' --run-command "docker pull redis" --run-command "docker pull nginx"
+        echo "Installing PyCloudStack required packages on host system"
+        apt install -y python3-virtualenv python3-libvirt libguestfs-dev libvirt-dev python3-dev net-tools
+        usermod -aG libvirt root
+        systemctl restart libvirtd
+        echo "Setting up PyCloudStack venv"
+        [ -d $TDX_TOOLS_DIR ] && rm -rf $TDX_TOOLS_DIR
+        git clone https://github.com/anjalirai-intel/tdx-tools.git $TDX_TOOLS_DIR
+        cd $TDX_TOOLS_DIR/tests/
+        source setupenv.sh
+        ssh-keygen -f tests/vm_ssh_test_key -N ""
+        cat >artifacts.yaml <<EOL
   latest-guest-image-ubuntu:
    source: file://${QCOW2_IMG}
   latest-guest-kernel-ubuntu:
    source: file://${VMLINUZ}
 EOL
 
-	cat artifacts.yaml
+        cat artifacts.yaml
 }
 
 run_pycloudstack(){
-	TEST_TYPE=$1
-	cd $TDX_TOOLS_DIR/tests/
-	if [[ $TEST_TYPE == "sanity" ]]; then
-		sudo ./run.sh -g ubuntu -c tests/test_tdvm_lifecycle.py
-	else
-		sudo ./run.sh -g ubuntu -s all
-	fi
+        TEST_TYPE=$1
+        cd $TDX_TOOLS_DIR/tests/
+        if [[ $TEST_TYPE == "sanity" ]]; then
+                ./run.sh -g ubuntu -c tests/test_tdvm_lifecycle.py
+        else
+                ./run.sh -g ubuntu -s all
+        fi
+        if [ $? -ne 0 ]; then
+                exit 1
+        fi
 }
 
 while true; do
@@ -159,8 +161,8 @@ while true; do
                 --createtd ) echo "createtd got selected"; createtd ;shift ;;
                 --runtdqemu ) echo "runtdqemu got selected"; runtdqemu ;shift ;;
                 --runtdlibvirt ) echo "runtdlibvirt got selected"; runtdlibvirt ;shift ;;
-                --smoke ) echo "Verify entire TDX and TD guest configuraiton"; setuptdx; verifytdx; createtd; runtdqemu; runtdlibvirt ;shift ;;
-		--automatedtests  ) echo "Pycloudstack automated tests got selected"; setuptdx; verifytdx; createtd; setup_pycloudstack; run_pycloudstack $2 ;shift ;;
+                --smoke ) echo "Verify entire TDX and TD guest configuraiton"; createtd; runtdqemu; runtdlibvirt ;shift ;;
+                --automatedtests ) echo "Pycloudstack automated tests got selected"; createtd; setup_pycloudstack; run_pycloudstack $2 ;shift ;;
                 -- ) shift; break;;
                 * ) break;;
         esac
