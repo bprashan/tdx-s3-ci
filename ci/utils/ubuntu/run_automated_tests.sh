2bca9f82cf36fdeba3aeabbd97596159b7bfd76d
B, Prashanth Natraj <Prashanth.Natraj.B@intel.com>
2024-09-09 11:34:37 +0530

    Run automation suites only on noble-24.04 branch - code refactoring
    Signed-off-by: B, Prashanth Natraj <Prashanth.Natraj.B@intel.com>

--- ci/utils/ubuntu/run_automated_tests.sh
+++ ci/utils/ubuntu/run_automated_tests.sh
@@ -1,47 +1,60 @@
+setup_pycloudstack() {
+    echo "Installing PyCloudStack required packages in Guest image"
+    # Install necessary packages in the guest image and run initial setup commands
+    LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1 virt-customize -a $QCOW2_IMG --install qemu-utils,libguestfs-tools,cpuid,python3-virtualenv,python3-libvirt,libguestfs-dev,libvirt-dev,python3-dev,net-tools,qemu-guest-agent,docker.io,cgroupfs-mount,python3-pip \
+        --run-command 'cgroupfs-mount' \
+        --run-command 'dockerd -D &' \
+        --run-command "sed -i 's/169.254.2.3/127.0.0.53/g' /etc/resolv.conf"
 
-setup_pycloudstack(){
-        echo "Installing PyCloudStack required packages in Guest image"
-        LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1 virt-customize -a $QCOW2_IMG --install qemu-utils,libguestfs-tools,cpuid,python3-virtualenv,python3-libvirt,libguestfs-dev,libvirt-dev,python3-dev,net-tools,qemu-guest-agent,docker.io,cgroupfs-mount,python3-pip --run-command 'cgroupfs-mount' --run-command 'dockerd -D &' --run-command "sed -i 's/169.254.2.3/127.0.0.53/g' /etc/resolv.conf"
-        echo "Installing PyCloudStack required packages on host system"
-        apt install -y python3-virtualenv python3-libvirt libguestfs-dev libvirt-dev python3-dev net-tools
-        usermod -aG libvirt root
-        systemctl restart libvirtd
-        echo "Setting up PyCloudStack venv"
-        [ -d $TDX_TOOLS_DIR ] && rm -rf $TDX_TOOLS_DIR
-        git clone https://github.com/anjalirai-intel/tdx-tools.git $TDX_TOOLS_DIR
-        cd $TDX_TOOLS_DIR/tests/
-        source setupenv.sh
-        ssh-keygen -f tests/vm_ssh_test_key -N ""
-        cat >artifacts.yaml <<EOL
-  latest-guest-image-ubuntu:
-   source: file://${QCOW2_IMG}
-  latest-guest-kernel-ubuntu:
-   source: file://${VMLINUZ}
+    echo "Installing PyCloudStack required packages on host system"
+    # Install necessary packages on the host system
+    apt install -y python3-virtualenv python3-libvirt libguestfs-dev libvirt-dev python3-dev net-tools
+    usermod -aG libvirt root
+    systemctl restart libvirtd
+
+    echo "Setting up PyCloudStack venv"
+    # Clone the tdx-tools repository and set up the virtual environment
+    [ -d $TDX_TOOLS_DIR ] && rm -rf $TDX_TOOLS_DIR
+    git clone https://github.com/anjalirai-intel/tdx-tools.git $TDX_TOOLS_DIR
+    cd $TDX_TOOLS_DIR/tests/
+    source setupenv.sh
+    ssh-keygen -f tests/vm_ssh_test_key -N ""
+
+    # Create artifacts.yaml with paths to the guest image and kernel
+    cat >artifacts.yaml <<EOL
+latest-guest-image-ubuntu:
+  source: file://${QCOW2_IMG}
+latest-guest-kernel-ubuntu:
+  source: file://${VMLINUZ}
 EOL
 
-        cat artifacts.yaml
+    cat artifacts.yaml
 }
 
-run_pycloudstack(){
-        TEST_TYPE=$1
-        cd $TDX_TOOLS_DIR/tests/
-        if [[ $TEST_TYPE == "sanity" ]]; then
-                ./run.sh -g ubuntu -c tests/test_tdvm_lifecycle.py
-        else
-                ./run.sh -g ubuntu -s all
-        fi
-        if [ $? -ne 0 ]; then
-                exit 1
-        fi
+run_pycloudstack() {
+    TEST_TYPE=$1
+    cd $TDX_TOOLS_DIR/tests/
+    # Run tests based on the TEST_TYPE parameter
+    if [[ $TEST_TYPE == "sanity" ]]; then
+        ./run.sh -g ubuntu -c tests/test_tdvm_lifecycle.py
+    else
+        ./run.sh -g ubuntu -s all
+    fi
+    # Exit if tests fail
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 }
 
-setup_canonical_suite(){
-        echo "Installing Canonical suite required packages on host system"
-        apt install tox python3
+setup_canonical_suite() {
+    echo "Installing Canonical suite required packages on host system"
+    # Install necessary packages for the Canonical suite
+    apt install -y tox python3
 }
 
-run_canonical_suite(){
-        cd $TDX_DIR/tests/tests
-        export TDXTEST_GUEST_IMG=$QCOW2_IMG
-        tox -e test_specify -- "not tdreport and not perf_benchmark"
+run_canonical_suite() {
+    cd $TDX_DIR/tests/tests
+    export TDXTEST_GUEST_IMG=$QCOW2_IMG
+    # Run Canonical suite tests excluding tdreport and perf_benchmark
+    tox -e test_specify -- "not tdreport and not perf_benchmark"
 }
--- ci/utils/ubuntu/run_smoke_test.sh
+++ ci/utils/ubuntu/run_smoke_test.sh
@@ -1,114 +1,141 @@
-setuptdx(){
-        [ -d $TDX_DIR ] && rm -rf $TDX_DIR
-        git clone -b $BRANCH_NAME https://github.com/canonical/tdx.git $TDX_DIR
-        cd $TDX_DIR
-        sed -i 's/^TDX_SETUP_ATTESTATION=0/TDX_SETUP_ATTESTATION=1/' "$TDX_DIR"/setup-tdx-config
-        ./setup-tdx-host.sh | tee setup_tdx.log
-        if [ $? -ne 0 ]; then
-                echo -e "\n\n ERROR: Setup TDX Host failed"
-                exit 1
-        fi
-        if grep -viq "${RESTART_CHECK_STRING}" setup_tdx.log; then
-                echo "system restart skipped"
-        else
-                echo "system restart required"
-                exit 3
-        fi
+# Function to setup TDX Host
+setuptdx() {
+    # Remove existing TDX directory if it exists
+    [ -d "$TDX_DIR" ] && rm -rf "$TDX_DIR"
+    
+    # Clone the TDX repository
+    git clone -b "$BRANCH_NAME" https://github.com/canonical/tdx.git "$TDX_DIR"
+    cd "$TDX_DIR"
+    
+    # Enable TDX attestation
+    sed -i 's/^TDX_SETUP_ATTESTATION=0/TDX_SETUP_ATTESTATION=1/' "$TDX_DIR/setup-tdx-config"
+    
+    # Run the setup script and log the output
+    ./setup-tdx-host.sh | tee setup_tdx.log
+    if [ $? -ne 0 ]; then
+        echo -e "\n\n ERROR: Setup TDX Host failed"
+        exit 1
+    fi
+    
+    # Check if system restart is required
+    if grep -viq "${RESTART_CHECK_STRING}" setup_tdx.log; then
+        echo "system restart skipped"
+    else
+        echo "system restart required"
+        exit 3
+    fi
 }
 
-verifytdx(){
-        var="$(dmesg | grep -i tdx)"
-        echo "$var"
-        if [[ "$var" =~ "${TDT_HOST_VERIFY_TEXT}" ]]; then
-                echo "tdx is configured on the Host"
-        else
-                echo "tdx is not configured on the Host"
-                exit 1
-        fi
+# Function to verify TDX Host configuration
+verifytdx() {
+    var="$(dmesg | grep -i tdx)"
+    echo "$var"
+    if [[ "$var" =~ "${TDT_HOST_VERIFY_TEXT}" ]]; then
+        echo "tdx is configured on the Host"
+    else
+        echo "tdx is not configured on the Host"
+        exit 1
+    fi
 }
 
-createtd(){
-        echo $GUEST_IMG_DIR
-        cd "$GUEST_IMG_DIR"
-        if [ -e td_image_created ]; then
-                echo "TD image already present"
-        else
-                ./create-td-image.sh
-                virt-get-kernel -a $QCOW2_IMG
-                touch td_image_created
-        fi
-        VMLINUZ="$GUEST_IMG_DIR"/"$(ls | grep vmlinuz)"
-        echo "$VMLINUZ"
-        cd "$TDX_DIR"
+# Function to create TD image
+createtd() {
+    echo "$GUEST_IMG_DIR"
+    cd "$GUEST_IMG_DIR"
+    
+    # Check if TD image already exists
+    if [ -e td_image_created ]; then
+        echo "TD image already present"
+    else
+        ./create-td-image.sh
+        virt-get-kernel -a "$QCOW2_IMG"
+        touch td_image_created
+    fi
+    
+    VMLINUZ="$GUEST_IMG_DIR/$(ls | grep vmlinuz)"
+    echo "$VMLINUZ"
+    cd "$TDX_DIR"
 }
 
-verifytd(){
-        echo "with portnumber : $1"
-        home_dir=$(cat /etc/passwd | grep $USER | cut -d ":" -f 6)
-        if [ -f "$home_dir/.ssh/known_hosts" ]; then
-                ssh-keygen -f "$home_dir/.ssh/known_hosts" -R "[localhost]:$TD_GUEST_PORT"
-        fi
-        out=$(sshpass -p $TD_GUEST_PASSWORD ssh -o StrictHostKeyChecking=no -p $1 root@localhost 'dmesg | grep -i tdx' 2>&1 )
-        echo "$out"
-        if [[ "$out" =~ "${TD_GUEST_VERIFY_TEXT}" ]]; then
-                echo "td guest is configured"
-        else
-                echo "td guest is not configured"
-                exit 1
-        fi
+# Function to verify TD Guest configuration
+verifytd() {
+    echo "with port number: $1"
+    home_dir=$(grep "$USER" /etc/passwd | cut -d ":" -f 6)
+    
+    # Remove existing SSH key for localhost
+    if [ -f "$home_dir/.ssh/known_hosts" ]; then
+        ssh-keygen -f "$home_dir/.ssh/known_hosts" -R "[localhost]:$TD_GUEST_PORT"
+    fi
+    
+    out=$(sshpass -p "$TD_GUEST_PASSWORD" ssh -o StrictHostKeyChecking=no -p "$1" root@localhost 'dmesg | grep -i tdx' 2>&1)
+    echo "$out"
+    if [[ "$out" =~ "${TD_GUEST_VERIFY_TEXT}" ]]; then
+        echo "td guest is configured"
+    else
+        echo "td guest is not configured"
+        exit 1
+    fi
 }
 
-cleanup(){
-        ./$TD_VIRSH_DELETE_CMD
-        fuser -k $QCOW2_IMG
-        sleep 20
+# Function to clean up existing TD guest instances
+cleanup() {
+    ./$TD_VIRSH_DELETE_CMD
+    fuser -k "$QCOW2_IMG"
+    sleep 20
 }
 
-runtdqemu(){
-        echo "creating TD guest with QEMU"
-        usermod -aG kvm $USER
-        cd "$GUEST_TOOLS_DIR"
-        cleanup
-        var=$( ./run_td.sh)
-        ret=$?
-        echo $var
-        if [ $ret -ne 0 ]; then
-                exit 1
-        fi
-        echo "verifying TD guest on QEMU"
-        port_num=$(echo $var | awk -F '-p' '{print $2}' | cut -d ' ' -f 2)
-        verifytd $port_num
-        QEMU_PID=$(echo $var | awk -F ', PID:' '{print $2}' | cut -d ' ' -f 2 | sed 's/,/ /g')
+# Function to run TD guest with QEMU
+runtdqemu() {
+    echo "creating TD guest with QEMU"
+    usermod -aG kvm "$USER"
+    cd "$GUEST_TOOLS_DIR"
+    cleanup
+    var=$(./run_td.sh)
+    ret=$?
+    echo "$var"
+    if [ $ret -ne 0 ]; then
+        exit 1
+    fi
+    echo "verifying TD guest on QEMU"
+    port_num=$(echo "$var" | awk -F '-p' '{print $2}' | cut -d ' ' -f 2)
+    verifytd "$port_num"
+    QEMU_PID=$(echo "$var" | awk -F ', PID:' '{print $2}' | cut -d ' ' -f 2 | sed 's/,/ /g')
 }
 
-cleantdqemu(){
-        echo "Killing Qemu with PID $QEMU_PID"
-        kill -9 $QEMU_PID
+# Function to clean TD guest with QEMU
+cleantdqemu() {
+    echo "Killing Qemu with PID $QEMU_PID"
+    kill -9 "$QEMU_PID"
 }
 
-runtdlibvirt(){
-        echo "creating TD guest with libvirt"
-        grep -q 'user =' "$LIBVIRT_CONF" && sed 's/^user =.*/user = \"root\"/' -i "$LIBVIRT_CONF" || sh -c "echo 'user = "root"' >> $LIBVIRT_CONF"
-        grep -q 'group =' "$LIBVIRT_CONF" && sed 's/^group =.*/group = \"root\"/' -i "$LIBVIRT_CONF" || sh -c "echo 'group = "root"' >> $LIBVIRT_CONF"
-        grep -q 'dynamic_ownership =' "$LIBVIRT_CONF" && sed 's/^dynamic_ownership =.*/dynamic_ownership = 0/' -i "$LIBVIRT_CONF" || sh -c "echo 'dynamic_ownership = 0' >> $LIBVIRT_CONF"
-        systemctl restart libvirtd
-        cd "$GUEST_TOOLS_DIR"
-        cleanup
-        var=$(./${TD_VIRSH_BOOT_CMD})
-        ret=$?
-        echo $var
-        if [ $ret -ne 0 ]; then
-                exit 1
-        fi
-        sleep 20
-        echo "verifying TD guest on libvirt"
-        if [[ $DISTRO_VER == "23.10" ]]; then
-                port_num=$(echo $var | awk -F '-p' '{print $2}' | cut -d ' ' -f 2)
-        else
-                port_num=$(echo $(./tdvirsh list --all) | awk -F 'ssh:' '{print $2}' | cut -d ',' -f 1)
-                vm_name=$(echo $var | awk -F 'Name:' '{print $2}' | cut -d ' ' -f 2)
-        fi
-        verifytd $port_num
-        del_vm=$(./tdvirsh delete ${vm_name})
-        echo $del_vm
+# Function to run TD guest with Libvirt
+runtdlibvirt() {
+    echo "creating TD guest with libvirt"
+    
+    # Update libvirt configuration
+    grep -q 'user =' "$LIBVIRT_CONF" && sed 's/^user =.*/user = "root"/' -i "$LIBVIRT_CONF" || echo 'user = "root"' >> "$LIBVIRT_CONF"
+    grep -q 'group =' "$LIBVIRT_CONF" && sed 's/^group =.*/group = "root"/' -i "$LIBVIRT_CONF" || echo 'group = "root"' >> "$LIBVIRT_CONF"
+    grep -q 'dynamic_ownership =' "$LIBVIRT_CONF" && sed 's/^dynamic_ownership =.*/dynamic_ownership = 0/' -i "$LIBVIRT_CONF" || echo 'dynamic_ownership = 0' >> "$LIBVIRT_CONF"
+    
+    systemctl restart libvirtd
+    cd "$GUEST_TOOLS_DIR"
+    cleanup
+    var=$(./${TD_VIRSH_BOOT_CMD})
+    ret=$?
+    echo "$var"
+    if [ $ret -ne 0 ]; then
+        exit 1
+    fi
+    sleep 20
+    echo "verifying TD guest on libvirt"
+    
+    if [[ $DISTRO_VER == "23.10" ]]; then
+        port_num=$(echo "$var" | awk -F '-p' '{print $2}' | cut -d ' ' -f 2)
+    else
+        port_num=$(echo $(./tdvirsh list --all) | awk -F 'ssh:' '{print $2}' | cut -d ',' -f 1)
+        vm_name=$(echo "$var" | awk -F 'Name:' '{print $2}' | cut -d ' ' -f 2)
+    fi
+    verifytd "$port_num"
+    del_vm=$(./tdvirsh delete "${vm_name}")
+    echo "$del_vm"
 }
